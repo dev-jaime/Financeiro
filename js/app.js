76c7d5670053b44a2ad7d32c79caa3898e688d50
dev-jaime <jaime.mendes.ixia@gmail.com>
2025-10-04 18:21:26 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,5 @@
 /* Funcoes utilitarias */
-function formatCurrencyBRL(valor) {
-  return Number(valor ?? 0).toLocaleString("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  });
-}
-
-function formatDate(v) {
-  if (!v) return "-";
-  try {
-    if (typeof v.toDate === "function") return v.toDate().toLocaleDateString("pt-BR");
-    return new Date(v).toLocaleDateString("pt-BR");
-  } catch {
-    return v;
-  }
-}
-
+import * as Utils from './utils.js';
 /* Funcoes de banco de dados */
 import { getReceitas } from "./receitas.js";
 
