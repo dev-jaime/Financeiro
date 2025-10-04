8d5c33458195eabed061992df53f545446130886
dev-jaime <jaime.mendes.ixia@gmail.com>
2025-10-04 15:05:35 -0500

    Update receitas.js

--- js/receitas.js
+++ js/receitas.js
@@ -1,5 +1,5 @@
 // js/receitas.js
-import { db } from "./firebase.js";
+import { db } from "firebase.js";
 import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
 // Ler todas as receitas
