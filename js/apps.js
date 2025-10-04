e21df0bbc88447f638c19224c3a241597eabd3ea
dev-jaime <jaime.mendes.ixia@gmail.com>
2025-10-04 14:30:23 -0500

    Create apps.js

---
+++ js/apps.js
@@ -0,0 +1,25 @@
+import { getReceitas } from "./receitas.js";
+
+async function carregar() {
+  const receitas = await getReceitas();
+  renderGrid(receitas);
+}
+
+function renderGrid(receitas) {
+  const tbody = document.querySelector("#gridReceitas tbody");
+  tbody.innerHTML = ""; // limpa antes de renderizar
+
+  receitas.forEach(r => {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+      <td>${r.descricao ?? ""}</td>
+      <td>${Number(r.valor ?? 0).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</td>
+      <td>${r.data ?? ""}</td>
+    `;
+
+    tbody.appendChild(tr);
+  });
+}
+
+carregar();
