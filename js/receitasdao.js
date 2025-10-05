dc8f527ebee7bc4ec331ae94b28fcfaa1cbcbd93
dev-jaime <jaime.mendes.ixia@gmail.com>
2025-10-04 20:16:32 -0500

    enviar

--- js/receitasdao.js
+++ js/receitasdao.js
@@ -2,13 +2,16 @@
 import { db } from "./firebase.js";
 import { collection, getDocs, addDoc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
+// referência à coleção "receitas"
+const colReceitas = collection(db, "Receitas");
+
 // Retorna todas as receitas
 export async function getReceitas() {
-  const snapshot = await getDocs(collection(db, "Receitas"));
+  const snapshot = await getDocs(colReceitas);
   return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 }
 
 export async function addReceita(data) {
-  const docRef = await addDoc(colecaoReceitas, data);
+  const docRef = await addDoc(colReceitas, data);
   return docRef.id;
 }
