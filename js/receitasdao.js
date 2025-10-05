79e903f8e0e2f1979e2a54c945a8309601d3700c
dev-jaime <jaime.mendes.ixia@gmail.com>
2025-10-04 20:04:15 -0500

    Update receitasdao.js

--- js/receitasdao.js
+++ js/receitasdao.js
@@ -1,6 +1,6 @@
 // js/receitas.js
 import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
+import { collection, getDocs, addDoc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
 
 // Retorna todas as receitas
 export async function getReceitas() {
