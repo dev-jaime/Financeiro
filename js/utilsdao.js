e174e20465734f3b5a1b59ea1e9ca40783d097fe
dev-jaime <jaime.mendes.ixia@gmail.com>
2025-10-05 15:32:25 -0500

    v

--- js/utilsdao.js
+++ js/utilsdao.js
@@ -1,14 +1,15 @@
 import { collection, query, where, orderBy, getDocs, addDoc, updateDoc, deleteDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
-export { collection, query, where, orderBy, getDocs, addDoc, updateDoc, deleteDoc, serverTimestamp };
 
 export async function prepareIdData(db, collectionName, data) {
-  const colRef = db.collection(collectionName);
+  const colRef = collection(db, collectionName);
 
   // busca último id da coleção
-  const lastQuery = await colRef.orderBy("id", "desc").limit(1).get();
+  const q = query(colRef, orderBy("id", "desc"), limit(1));
+  const snapshot = await getDocs(q);
+
   let newId = 1;
-  if (!lastQuery.empty) {
-    const lastDoc = lastQuery.docs[0];
+  if (!snapshot.empty) {
+    const lastDoc = snapshot.docs[0];
     newId = (lastDoc.data().id || 0) + 1;
   }
 
