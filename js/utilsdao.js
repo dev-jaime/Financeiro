003295441eb7c892951a4cdb0f117ccd2dcdbd8e
dev-jaime <jaime.mendes.ixia@gmail.com>
2025-10-05 15:54:34 -0500

    p

--- js/utilsdao.js
+++ js/utilsdao.js
@@ -1,6 +1,6 @@
 import { db } from "./firebase.js";
 import { collection, limit, query, where, orderBy, getDocs, addDoc, setDoc, updateDoc, deleteDoc, serverTimestamp, doc } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-firestore.js";
-export { db, collection, limit, query, where, orderBy, getDocs, addDoc, updateDoc, deleteDoc, serverTimestamp, doc }
+export { db, collection, limit, query, where, orderBy, getDocs, addDoc, setDoc, updateDoc, deleteDoc, serverTimestamp, doc }
 
 export async function prepareIdData(db, collectionName, data) {
   const colRef = collection(db, collectionName);
